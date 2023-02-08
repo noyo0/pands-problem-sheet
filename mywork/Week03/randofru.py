f72037b56712fb4b35a35098976a74355e920ab1
Noyo0 <norbert.antaloutlook.com>
2023-02-08 21:22:37 +0000

    cont...

--- mywork/Week03/randofru.py
+++ mywork/Week03/randofru.py
@@ -3,4 +3,4 @@ fruits = ['Apple', 'Orange', 'Banana', 'Pear']
 # we want a random number between 0 and lenght-1
 index = random.randint(0,len(fruits)-1)
 fruit = fruits[index]
-print("A Random Fruit:{}".format(fruit))
+print(f"A Random Fruit:{fruit}")
