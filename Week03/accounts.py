eef78009305caf85e43f5eb4ecb08182b8f618da
Noyo0 <norbert.antaloutlook.com>
2023-02-09 19:54:30 +0000

    tidy up and add extra version

--- Week03/accounts.py
+++ Week03/accounts.py
@@ -5,8 +5,8 @@
 # Write a python program called accounts.py that reads in a 10 character account number and outputs the account number with only the last 4 digits showing (and the first 6 digits replaced with Xs).
 
 # prompt for account number
-accno = int(input("Please enter an 10 digit account number: "))
+accno = int(input("Please enter your 10 digit account number: "))
 #calculate last 4 digit by taking remainder of division by 10000
 last4 = str(accno % 10000)
-#combine XXXXXX and last 4 digits to conceal full account number 
+#combine 6X and last 4 digits to conceal full account number 
 print(f"XXXXXX{last4}")
--- Week03/accounts3anylenghtanything.py
+++ Week03/accounts3anylenghtanything.py
@@ -4,12 +4,13 @@
 # Bank account numbers can stored as 10 character strings, for security reasons some applications only display the last 4 characters (with the other other characters replaced with Xs).
 # Write a python program called accounts.py that reads in a 10 character account number and outputs the account number with only the last 4 digits showing (and the first 6 digits replaced with Xs).
 # Modify the program to deal with account numbers of any length (comment your assumptions)
+# same but works with letters too
 
 # prompt for account number
-accno = int(input("Please enter your account number of any lenght: "))
-#calculate last 4 digit by taking remainder of division by 10000 // and convert to string so len() can work
-last4 = str(accno % 10000)
+accno = (input("Please enter whatever you want: "))
 #check lenght of original account number (source ref: https://www.geeksforgeeks.org/python-string-length-len/) and take 4 to get the number of "X" we need to cover the concealed part of the account number
 coverlenght = len(str(accno))-4
-#combine account number lenght -4 X letters and last 4 digits of account number to conceal full account number
-print(coverlenght * "X" + (last4))
+#convert accno to string
+accstr = str(accno)
+#display an amount of "X" equal to acc number lenght minus 4 and select last 4 characters from account number as string (this allows for letters in the account number) to display last 4 digits/characters
+print(coverlenght * "X" + accstr[(coverlenght):(coverlenght)+4]) #found a nicer slicer (source ref: https://www.interviewqs.com/ddi-code-snippets/substring-python)
