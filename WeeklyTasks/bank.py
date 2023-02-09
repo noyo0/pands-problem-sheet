431fc43c5b5b1d9e85abcd638ffe0c50d0c201e4
Noyo0 <norbert.antaloutlook.com>
2023-02-09 18:21:48 +0000

    tidy up comments

--- WeeklyTasks/bank.py
+++ WeeklyTasks/bank.py
@@ -3,9 +3,10 @@
 # Author: Norbert Antal
 # Program functions:
 #   Prompt the user and read in two money amounts (in cent)
-#   Add the two amounts
 #   Print out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount 
 amount1 = int(input("Enter the first amount (in cents):\t "))
 amount2 = int(input("Enter the second amount (in cents):\t "))
+#   Add the two amounts and divide by 100 to convert cents to euros
 eur = (amount1+amount2)/100
+# display result in the requested format
 print(f"The sum of these is: \t\t\t€{eur}")
