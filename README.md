8be4207111035ec8769d334b40ea64304597023e
Noyo0 <norbert.antaloutlook.com>
2023-03-03 22:29:13 +0000

    Readme Update

--- README.md
+++ README.md
@@ -26,16 +26,27 @@
 >> Program asks user to input any positive integer and outputs the successive values of the following calculation; At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one. Program ends if the current value is one.
 >>    ###### * References: 
 >>###### 1. list item data types - ref: (https://www.w3schools.com/python/python_lists.asp)  
->>###### 2. print with custom dividers - ref: https://stackoverflow.com/questions/11178061/print-list-without-brackets-in-a-single-row)
+>>###### 2. print with custom dividers - ref: https://stackoverflow.com/questions/11178061/print-list-without-brackets-in-a-single-row
 
 - #### Week05
 > w05_weekday.py
->> Program checks if current date falls on a weekday or not
+>> Program checks if current date falls on a weekday or not, using the imported -datetime- module's date.weekday() function that returns the day of the week as an integer, where Monday is 0 and Sunday is 6. 
+An if statement determines wether the current day represented as integer is a weekend day (larger than 4) otherwise it must be a weekday. Based on the if statement the appropriate message is printed.
+There is no user interaction in this program.
 >>    ###### * References: 
 >>###### 1. datetime module - https://docs.python.org/3/library/datetime.html
 
 - #### Week06
 > w06_squareroot.py
->> Program checks if current date falls on a weekday or not
+>> Program finds approximate square root of a given number with Newton-Raphson Method
+Created function "fn_newton" with two arguments; number, display. 
+ "number" argument sets the number to calculate with, 
+ "display" argument determines if the result is displayed as floating point number (default:0) or an integer (1)
+Using the formula from reference below the program loops until maximum accuracy is reached;
+    I noticed that while allowing infinite iterations of the square root calculation, Python limits the number of digits at 17 so the results are repeating once 17-digit accuracy is achieved.
+    Exploiting this "feature" the program can store results in a container and check current result against previous iteration. 
+    If the two are the same, the result must be the most accurate within this 17-digit restriction and the program can break out of the loop as there is no need to iterate any more.
+As a failsafe in case for extreme large numbers a variable for maximum number of iterations is also established, with value set to 100.
+User interaction provides the starting number where the Program insists on a positive number if negative is given, user also choses if the result is printed as integer or floating point number
 >>    ###### * References: 
->>###### 1. datetime module - https://docs.python.org/3/library/datetime.html
+>>###### 1. Newton-Raphson Method: https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
--- w06_squareroot.py
+++ w06_squareroot.py
@@ -1,16 +1,17 @@
 # squareroot.py
 # Author: Norbert Antal
-# Program finds square root of a given number with Newton-Raphson Method
+# Program finds approximate square root of a given number with Newton-Raphson Method
 # Created function "fn_newton" with two arguments; number, display. 
 # "number" argument sets the number to calculate with, 
-# "display" argument determines if the result is displayed as floating point number (default:0) or an integer (1)
+# "display" argument determines if the result is displayed as integer (default:0) or a floating point number (1)
 # Program runs until max accuracy is reached;
-    # I noticed that while infinite iterations, python limits the number of digits at 17 and after reaching max accuracy within this 17 digit restraijnt, the results are repeating.
+    # I noticed that while allowing infinite iterations of the square root calculation, python limits the number of digits at 17 so the results are repeating once 17-digit accuracy is achieved.
     # Exploiting this "feature" the program can store results in a container and check current result against previous iteration. 
-    # If the two are the same, there is no need to iterate any more, the result is the most accurate and final the program can break out of the loop.
+    # If the two are the same, the result must be the most accurate within this 17-digit restriction and the program can break out of the loop as there is no need to iterate any more.
 # ref: Newton-Raphson Method: https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
 
-# ------------------------------fn_newton-------start--------
+# -------fn_newton------------------------start
+
 def fn_newton(number,disp=0):
     i=0
     itr=100 #maximum number of iterations in case of extreme large numbers (probably overkill)
@@ -26,15 +27,17 @@ def fn_newton(number,disp=0):
                 return(int(x)) # if user chose int, result returns as int
             else:
                 return(x)  # if user chose float (or left the second argument blank), result returns as float
-# -----------------------------fn_newton---------end-----------
+            
+# -------fn_newton------------------------end
 
+# user interaction ---------
 n=float(input("Please enter a positive floating-point number: ")) # user prompted for a positive floating point number
 while n<=0: # program will wex the user until positive number is given
     n=float(input("...please enter a positive floating-point number: ")) 
-f=str(input("Do you want the result as a floating point(Yes?): ")) # User prompted for display method
+f=str(input("Do you want the result as an integer (Yes?): ")) # User prompted for display method
 if f== "Y" or f=="y" or f=="yes" or f=="Yes": # Program allows for few types of "yes" entries for Floating point display, everything else results in integer display
-    f=0
-else:
     f=1
-p=fn_newton(n,f) # setting variable p for printing the result
-print(f"\n\nthe square root of {n} is approximately: {p}") #result is printed as per user interaction
+else:
+    f=0
+p=fn_newton(n,f) # setting value for "p" by calling function fn_newton with arguments defined by user interaction
+print(f"\n\n The square root of {n} is approximately: {p}") #starting number and approximate square root is printed as per user interaction
