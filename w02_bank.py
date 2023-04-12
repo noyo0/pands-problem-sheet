0765266ac0b7b3356f84ac16987a05af7400f6db
Noyo0 <norbert.antaloutlook.com>
2023-04-12 21:18:12 +0100

    w02_bank.py corrected after feedback

--- w02_bank.py
+++ w02_bank.py
@@ -12,11 +12,16 @@ amount2 = int(input("Enter the second amount (in cents):\t "))
 eur = (amount1+amount2)/100
 print(f"The sum of these is: \t\t\t€{eur}")'''
 
+# ☝️AFTER FEEDBACK
 # without floats
-add = str(amount1+amount2)
-eur = add[:-2]
-cnt = add[-2]
-# Display result in requested format
-print(f"The sum of these is: {add}\t\t\t{eur} Euros and {cnt} cents")
-#this is only working if the sum is min 100 cents unless I add an if statement
+add = amount1+amount2 # Add the two amounts and store sum in 'add'
+euro = add // 100 #Floor division - returns the closest whole number after division -> in this case the largest number divisible with 100 (100 cent = 1 euro) - store value in 'euro'
+cent = add % 100 #modulus operator - returns the remainder of above division. (cents) - store value in 'cents'
+# Integer and float division ref: https://www.educative.io/answers/what-is-integer-and-float-division-in-python
+
+# Display result in human readable format using the variables for formatting
+print(f'''
+You entered {amount1}c and {amount2}c, 
+{add} cents in total,
+which is {euro} Euros and {cent} cents.''')
 
